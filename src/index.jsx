39b78ab24b875bd6dd7950483ebef05672943ef5
Fernando <fer.oliveeira@hotmail.com>
2024-06-19 03:58:35 -0300

    build: links da navbar agora sao dinamicos

--- src/index.jsx
+++ src/index.jsx
@@ -5,6 +5,7 @@ import './global.css';
 import {BrowserRouter} from 'react-router-dom';
 import MainRoutes from './routes';
 import Navbar from './components/navbar';
+import { NAV_LINKS } from './contants';
 
 
 
@@ -12,7 +13,11 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <Navbar 
-      imgSrc = 'game_default.png'
+      
+      imgSrc = {JSON.parse(localStorage.userData).map((data) => data.profile_image_url)}
+      imgAlt = {JSON.parse(localStorage.userData).map((data) => data.login)}
+      navLinks = {NAV_LINKS}
+      
     />
     <MainRoutes/>
   </BrowserRouter>
